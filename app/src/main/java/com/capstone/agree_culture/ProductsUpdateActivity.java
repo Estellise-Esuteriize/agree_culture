284227e443427fbcb7047dd519044fc087440f9c
Estellise <shiranoyukihime@gmail.com>
2019-03-19 13:55:29 -0700

    created UI for add product

--- app/src/main/java/com/capstone/agree_culture/ProductsUpdateActivity.java
+++ app/src/main/java/com/capstone/agree_culture/ProductsUpdateActivity.java
@@ -7,14 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ProductsCreationActivity extends AppCompatActivity {
+public class ProductsUpdateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_products_creation);
-
-
+        setContentView(R.layout.activity_products_update);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
