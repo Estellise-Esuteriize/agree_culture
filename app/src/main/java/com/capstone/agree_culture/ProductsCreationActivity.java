f242bb451914c7305adfed25d899e5d507b525a0
Estellise <shiranoyukihime@gmail.com>
2019-03-20 20:07:00 +0800

    ------------

--- app/src/main/java/com/capstone/agree_culture/ProductsCreationActivity.java
+++ app/src/main/java/com/capstone/agree_culture/ProductsCreationActivity.java
@@ -6,14 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProductsCreationActivity extends AppCompatActivity {
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
-    }
+
+    private EditText product_name;
+    private EditText product_price;
+    private EditText product_quantity;
+    private EditText product_minimum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,27 @@ public class ProductsCreationActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
+        product_name = (EditText) findViewById(R.id.product_create_name);
+        product_price = (EditText) findViewById(R.id.product_create_price);
+        product_quantity = (EditText) findViewById(R.id.product_create_quantity);
+        product_minimum = (EditText) findViewById(R.id.product_create_minimum);
+
+
+
+
+
+
+
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+
+
+
 
 }
