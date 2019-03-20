f4f368fe0ebbce9a2e77e32afa13979a9575e029
Estellise <shiranoyukihime@gmail.com>
2019-03-20 23:19:59 +0800

    added product model:

--- app/src/main/java/com/capstone/agree_culture/ProductsCreationActivity.java
+++ app/src/main/java/com/capstone/agree_culture/ProductsCreationActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class ProductsCreationActivity extends AppCompatActivity {
 
@@ -16,6 +20,9 @@ public class ProductsCreationActivity extends AppCompatActivity {
     private EditText product_price;
     private EditText product_quantity;
     private EditText product_minimum;
+
+
+    private NumberFormat currentFormat;
 
 
     @Override
@@ -37,10 +44,10 @@ public class ProductsCreationActivity extends AppCompatActivity {
         product_quantity = (EditText) findViewById(R.id.product_create_quantity);
         product_minimum = (EditText) findViewById(R.id.product_create_minimum);
 
+        currentFormat = NumberFormat.getCurrencyInstance();
 
 
-
-
+        product_price.addTextChangedListener(new PriceCurrencyFormat());
 
 
     }
@@ -51,6 +58,43 @@ public class ProductsCreationActivity extends AppCompatActivity {
         return true;
     }
 
+
+
+    
+
+
+    class PriceCurrencyFormat implements TextWatcher{
+
+        private String current;
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(!s.toString().equals(current)){
+                product_price.removeTextChangedListener(this);
+
+                String cleanString = s.toString().replaceAll("[$,.]", "");
+
+                double parsed = Double.parseDouble(cleanString);
+                String formatted = NumberFormat.getCurrencyInstance().format((parsed/100));
+
+                current = formatted;
+                product_price.setText(formatted);
+                product_price.setSelection(formatted.length());
+
+                product_price.addTextChangedListener(this);
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    }
 
 
 
---
+++ app/src/main/java/com/capstone/agree_culture/model/Product.java
@@ -0,0 +1,65 @@
+package com.capstone.agree_culture.model;
+
+public class Product {
+
+
+    private String user_id;
+    private String product_name;
+    private Double product_price;
+    private Integer product_quantity;
+    private Integer product_minimum;
+
+
+    public Product(){
+
+    }
+
+    public Product(String user_id, String product_name, Double product_price, Integer product_quantity, Integer product_minimum){
+        this.user_id = user_id;
+        this.product_name = product_name;
+        this.product_price = product_price;
+        this.product_quantity = product_quantity;
+        this.product_minimum = product_minimum;
+    }
+
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public Double getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(Double product_price) {
+        this.product_price = product_price;
+    }
+
+    public Integer getProduct_quantity() {
+        return product_quantity;
+    }
+
+    public void setProduct_quantity(Integer product_quantity) {
+        this.product_quantity = product_quantity;
+    }
+
+    public Integer getProduct_minimum() {
+        return product_minimum;
+    }
+
+    public void setProduct_minimum(Integer product_minimum) {
+        this.product_minimum = product_minimum;
+    }
+}
