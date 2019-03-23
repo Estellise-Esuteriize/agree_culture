9f43bdf25ed2bd4e00562a018f660c33fcf205aa
Estellise <shiranoyukihime@gmail.com>
2019-03-24 02:17:44 +0800

    added global user product type

--- app/src/main/java/com/capstone/agree_culture/Helper/Helper.java
+++ app/src/main/java/com/capstone/agree_culture/Helper/Helper.java
@@ -9,4 +9,22 @@ public class Helper {
 
     public static User currentUser = null;
 
+
+    public static String userProductType(String user_product_type){
+
+        if(user_product_type.equals(GlobalString.CUSTOMER)){
+            return GlobalString.DISTRIBUTOR;
+        }
+        else if(user_product_type.equals(GlobalString.DISTRIBUTOR)){
+            return GlobalString.SUPPLIER;
+        }
+        else if(user_product_type.equals(GlobalString.SUPPLIER)){
+            return GlobalString.DISTRIBUTOR;
+        }
+
+        return null;
+
+    }
+
+
 }
