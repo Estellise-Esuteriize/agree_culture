4db1f34d23d6046911c888e218b783a415996bed
Estellise <shiranoyukihime@gmail.com>
2019-04-07 08:06:49 +0800

    update map marker

--- app/src/main/java/com/capstone/agree_culture/DeliveryDestinationMapActivity.java
+++ app/src/main/java/com/capstone/agree_culture/DeliveryDestinationMapActivity.java
@@ -1,30 +1,120 @@
 package com.capstone.agree_culture;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.capstone.agree_culture.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class DeliveryDestinationMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private Marker deliveryCar;
+
+    private final int REQUEST_LOCATION_PERMISSION = 101;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
+    private boolean requestingLocationUpdates;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_destination_map);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(20 * 1000);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+
+                        Log.d("Location Updates", location.getLatitude() + location.getLongitude() + "");
+
+                        if(mMap != null){
+
+
+                            LatLng lang = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            if(deliveryCar != null){
+                                deliveryCar.setPosition(lang);
+
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lang, 16.2f));
+
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+            }
+        };
+
+
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+        else{
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -41,7 +131,26 @@ public class DeliveryDestinationMapActivity extends FragmentActivity implements 
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+        deliveryCar = mMap.addMarker(new MarkerOptions().position(sydney).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_directions_car)));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case REQUEST_LOCATION_PERMISSION:
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+                }
+
+                break;
+        }
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
