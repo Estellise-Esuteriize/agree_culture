c2525ffc8e95351d1155d95a24f6b0120ccc6efe
Estesllise <shiranoyukihime@gmail.com>
2019-05-23 16:50:53 +0800

    Dfsgdlfjkgsldkgjsldgsdgf

--- app/src/androidTest/java/com/capstone/agree_culture/AccountLogOut.java
+++ app/src/androidTest/java/com/capstone/agree_culture/AccountLogOut.java
@@ -117,7 +117,7 @@ public class AccountLogOut {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(7000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
