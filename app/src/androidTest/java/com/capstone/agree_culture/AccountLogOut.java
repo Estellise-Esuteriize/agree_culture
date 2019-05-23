3316a63e69d73c3910c26c88e2c7b999224dde0f
Estesllise <shiranoyukihime@gmail.com>
2019-05-23 14:11:58 +0800

    sdfgljksdglsdkjglsdikgs'

--- app/src/androidTest/java/com/capstone/agree_culture/AccountLogOut.java
+++ app/src/androidTest/java/com/capstone/agree_culture/AccountLogOut.java
@@ -1,7 +1,9 @@
 package com.capstone.agree_culture;
 
 
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -41,87 +43,76 @@ public class AccountLogOut {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+
         try {
-            Thread.sleep(7000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ViewInteraction actionMenuItemView = onView(
+                    allOf(withId(R.id.home_login), withContentDescription("Login"),
+                            childAtPosition(
+                                    childAtPosition(
+                                            withId(R.id.toolbar),
+                                            1),
+                                    0),
+                            isDisplayed()));
+            actionMenuItemView.perform(click());
+
+            // Added a sleep statement to match the app's execution delay.
+            // The recommended way to handle such scenarios is to use Espresso idling resources:
+            // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ViewInteraction appCompatAutoCompleteTextView = onView(
+                    allOf(withId(R.id.email),
+                            childAtPosition(
+                                    childAtPosition(
+                                            withClassName(is("android.support.design.widget.TextInputLayout")),
+                                            0),
+                                    0)));
+            appCompatAutoCompleteTextView.perform(scrollTo(), replaceText("distributor@gmail.com"), closeSoftKeyboard());
+
+            ViewInteraction appCompatEditText = onView(
+                    allOf(withId(R.id.password),
+                            childAtPosition(
+                                    childAtPosition(
+                                            withClassName(is("android.support.design.widget.TextInputLayout")),
+                                            0),
+                                    0)));
+            appCompatEditText.perform(scrollTo(), click());
+
+            ViewInteraction appCompatEditText2 = onView(
+                    allOf(withId(R.id.password),
+                            childAtPosition(
+                                    childAtPosition(
+                                            withClassName(is("android.support.design.widget.TextInputLayout")),
+                                            0),
+                                    0)));
+            appCompatEditText2.perform(scrollTo(), replaceText("123123"), closeSoftKeyboard());
+
+
+            ViewInteraction appCompatButton = onView(
+                    allOf(withId(R.id.sign_in_button), withText("Sign in"),
+                            childAtPosition(
+                                    allOf(withId(R.id.email_login_form),
+                                            childAtPosition(
+                                                    withId(R.id.login_form),
+                                                    0)),
+                                    2)));
+            appCompatButton.perform(scrollTo(), click());
+
         }
-
-        ViewInteraction actionMenuItemView = onView(
-                allOf(withId(R.id.home_login), withContentDescription("Login"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.toolbar),
-                                        1),
-                                0),
-                        isDisplayed()));
-        actionMenuItemView.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(7000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        catch (NoMatchingViewException ex) {
+            ex.printStackTrace();
         }
-
-        ViewInteraction appCompatAutoCompleteTextView = onView(
-                allOf(withId(R.id.email),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.design.widget.TextInputLayout")),
-                                        0),
-                                0)));
-        appCompatAutoCompleteTextView.perform(scrollTo(), replaceText("distributor@gmail.c"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.password),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.design.widget.TextInputLayout")),
-                                        0),
-                                0)));
-        appCompatEditText.perform(scrollTo(), click());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.password),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.design.widget.TextInputLayout")),
-                                        0),
-                                0)));
-        appCompatEditText2.perform(scrollTo(), replaceText("123123"), closeSoftKeyboard());
-
-        ViewInteraction appCompatAutoCompleteTextView2 = onView(
-                allOf(withId(R.id.email), withText("distributor@gmail.c"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.design.widget.TextInputLayout")),
-                                        0),
-                                0)));
-        appCompatAutoCompleteTextView2.perform(scrollTo(), replaceText("distributor@gmail.com"));
-
-        ViewInteraction appCompatAutoCompleteTextView3 = onView(
-                allOf(withId(R.id.email), withText("distributor@gmail.com"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.design.widget.TextInputLayout")),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatAutoCompleteTextView3.perform(closeSoftKeyboard());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.sign_in_button), withText("Sign in"),
-                        childAtPosition(
-                                allOf(withId(R.id.email_login_form),
-                                        childAtPosition(
-                                                withId(R.id.login_form),
-                                                0)),
-                                2)));
-        appCompatButton.perform(scrollTo(), click());
-
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -142,15 +133,9 @@ public class AccountLogOut {
                         isDisplayed()));
         appCompatImageButton.perform(click());
 
-        ViewInteraction navigationMenuItemView = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.design_navigation_view),
-                                childAtPosition(
-                                        withId(R.id.nav_view),
-                                        0)),
-                        10),
-                        isDisplayed()));
-        navigationMenuItemView.perform(click());
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_logout));
+
 
         ViewInteraction appCompatButton2 = onView(
                 allOf(withId(android.R.id.button1), withText("OK"),
@@ -160,6 +145,13 @@ public class AccountLogOut {
                                         0),
                                 3)));
         appCompatButton2.perform(scrollTo(), click());
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
     }
 
     private static Matcher<View> childAtPosition(
